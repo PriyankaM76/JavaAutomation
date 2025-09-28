ba35f0cf63d8fa5297d79629dd0264359927c6a4
Priyanka <mishr76pm@gmail.com>
2025-09-28 16:11:15 +0530

    added sout

--- src/ex_01_JavaPrograms/Lab002_Test1.java
+++ src/ex_01_JavaPrograms/Lab002_Test1.java
@@ -1,4 +1,8 @@
 package ex_01_JavaPrograms;
 
 public class Lab002_Test1 {
+    public static void main()
+    {
+        System.out.println("new program");
+    }
 }
--- src/ex_01_JavaPrograms/Lab002_Test2.java
+++ src/ex_01_JavaPrograms/Lab002_Test2.java
@@ -1,4 +1,7 @@
 package ex_01_JavaPrograms;
 
 public class Lab002_Test2 {
+    public static void main() {
+        System.out.println("new program");
+    }
 }
